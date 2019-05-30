dc79e020946a46cc193df6b00a00e75c4bede683
yuanxs <yuanxs@95szzl.cn>
2019-05-30 14:30:38 +0800

    201905301430

--- Common.go
+++ Common.go
@@ -2,100 +2,118 @@ package goToolSecret
 
 import (
 	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
 	"encoding/base64"
-	"strings"
-	"sync"
+	"encoding/hex"
+	"github.com/Deansquirrel/goToolCommon"
+	"github.com/kataras/iris/core/errors"
 )
 
-var key []byte
+//将明文加密为Base64格式的密文
+//plainText原文
+//key密码
+func EncryptToBase64Format(plainText string, key string) (string, error) {
+	sMD5 := goToolCommon.Md5([]byte(plainText + key))
 
-var rwLock sync.RWMutex
-
-func init() {
-	SetCode("Deansquirrel521247944341")
-}
-
-func SetCode(code string) {
-	rwLock.Lock()
-	defer rwLock.Unlock()
-	code = strings.Trim(code, " ")
-	if code == "" {
-		key = nil
-	} else {
-		key = []byte(code)
+	var bufferResult bytes.Buffer
+	{
+		r, err := hex.DecodeString(sMD5)
+		if err != nil {
+			return "", err
+		}
+		_, err = bufferResult.Write(r)
+		if err != nil {
+			return "", err
+		}
+		_, err = bufferResult.Write([]byte(key))
+		if err != nil {
+			return "", err
+		}
+		_, err = bufferResult.Write([]byte(plainText))
+		if err != nil {
+			return "", err
+		}
 	}
+	resultByte := bufferResult.Bytes()
+
+	keyByte := []byte(key)
+	for index := range resultByte {
+		resultByte[index] = resultByte[index] ^ (keyByte[index%len(keyByte)])
+	}
+
+	var rndKey [4]byte
+	for i := 0; i < 4; i++ {
+		rndKey[i] = uint8(goToolCommon.RandInt(0, 64))
+	}
+
+	for index := range resultByte {
+		resultByte[index] = resultByte[index] ^ (rndKey[index%4])
+	}
+
+	var bufferFinally bytes.Buffer
+	{
+		_, err := bufferFinally.Write(rndKey[0:])
+		if err != nil {
+			return "", err
+		}
+		_, err = bufferFinally.Write(resultByte)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return base64.StdEncoding.EncodeToString(bufferFinally.Bytes()), nil
 }
 
-//加密内容作Base64编码
-func EncryptStr(str string) (string, error) {
-	b, err := Encrypt([]byte(str))
+//将Base64格式的密文解密
+//cipherText密文
+//key密码
+func DecryptFromBase64Format(cipherText string, key string) (string, error) {
+	byteCipherText, err := base64.StdEncoding.DecodeString(cipherText)
+	if err != nil {
+		//base64解码失败
+		return "", err
+	}
+
+	rndKey := byteCipherText[0:4]
+
+	resultByte := byteCipherText[4:]
+
+	for index := range resultByte {
+		resultByte[index] = resultByte[index] ^ (rndKey[index%4])
+	}
+
+	keyByte := []byte(key)
+	for index := range resultByte {
+		resultByte[index] = resultByte[index] ^ (keyByte[index%len(keyByte)])
+	}
+
+	checkKeyByte := resultByte[16 : 16+len(keyByte)]
+
+	if !bytes.Equal(keyByte, checkKeyByte) {
+		return "", errors.New("解密失败。（密码非法）")
+	}
+
+	sMD5Check := resultByte[0:16]
+	plainByte := resultByte[16+len(keyByte):]
+
+	var bufferTemp bytes.Buffer
+	_, err = bufferTemp.Write(plainByte)
 	if err != nil {
 		return "", err
 	}
-	return base64.StdEncoding.EncodeToString(b), nil
-}
-
-//解密Base64编码
-func DecryptStr(str string) (string, error) {
-	b, err := base64.StdEncoding.DecodeString(str)
+	_, err = bufferTemp.Write(keyByte)
 	if err != nil {
 		return "", err
 	}
-	r, err := Decrypt(b)
+
+	sMD5 := goToolCommon.Md5(bufferTemp.Bytes())
+	r, err := hex.DecodeString(sMD5)
 	if err != nil {
 		return "", err
 	}
-	return string(r), nil
-}
 
-//加密
-func Encrypt(str []byte) ([]byte, error) {
-	rwLock.RLock()
-	defer rwLock.RUnlock()
-	if key == nil {
-		return str, nil
+	if !bytes.Equal(sMD5Check, r) {
+		return "", errors.New("解密失败。（校验错误）")
 	}
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	blockSize := block.BlockSize()
-	str = PKCS7Padding(str, blockSize)
-	blockModel := cipher.NewCBCEncrypter(block, key[:blockSize])
-	result := make([]byte, len(str))
-	blockModel.CryptBlocks(result, str)
-	return result, nil
-}
-
-//解密
-func Decrypt(str []byte) ([]byte, error) {
-	rwLock.RLock()
-	defer rwLock.RUnlock()
-	if key == nil {
-		return str, nil
-	}
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	blockSize := block.BlockSize()
-	blockModel := cipher.NewCBCDecrypter(block, key[:blockSize])
-	result := make([]byte, len(str))
-	blockModel.CryptBlocks(result, str)
-	result = PKCS7UnPadding(result)
-	return result, nil
-}
-
-func PKCS7Padding(cipherText []byte, blockSize int) []byte {
-	padding := blockSize - len(cipherText)%blockSize
-	padText := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(cipherText, padText...)
-}
-
-func PKCS7UnPadding(origData []byte) []byte {
-	length := len(origData)
-	unPadding := int(origData[length-1])
-	return origData[:(length - unPadding)]
+	return string(plainByte), nil
 }
